85f62efc5247005d88a74453d289db9af60a44de
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-02-17 18:19:15 +0300

    project object & project repository

---
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,9 @@
+package io.kuzin.ppmtool.repositories;
+
+import io.kuzin.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
