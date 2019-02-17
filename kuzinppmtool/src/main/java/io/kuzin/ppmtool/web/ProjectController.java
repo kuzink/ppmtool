72beaff5a5934afdc1b4647a504ed3f76d6c998b
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-02-17 23:01:40 +0300

    refactor project controller

--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/ProjectController.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/ProjectController.java
@@ -1,19 +1,17 @@
 package io.kuzin.ppmtool.web;
 
 import io.kuzin.ppmtool.domain.Project;
+import io.kuzin.ppmtool.services.MapValidationErrorService;
 import io.kuzin.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,17 +20,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService validationService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = validationService.validate(result);
+        if(errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
