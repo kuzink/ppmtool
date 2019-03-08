d16a7ece0b5767ed76cbc5da9cb9a12153c3b0cd
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-08 18:48:06 +0300

    update validation and delete

--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/ProjectTaskService.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/ProjectTaskService.java
@@ -9,6 +9,7 @@ import io.kuzin.ppmtool.repositories.ProjectRepository;
 import io.kuzin.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class ProjectTaskService {
@@ -81,9 +82,21 @@ public class ProjectTaskService {
 
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
 
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
         projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
+    }
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 }
