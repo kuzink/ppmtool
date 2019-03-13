af532c6822f2495e3469c4f19345f850094d68f3
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-13 22:13:46 +0300

    create User object, repository, service

---
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package io.kuzin.ppmtool.repositories;
+
+import io.kuzin.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
---
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/UserService.java
@@ -0,0 +1,14 @@
+package io.kuzin.ppmtool.services;
+
+import io.kuzin.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
