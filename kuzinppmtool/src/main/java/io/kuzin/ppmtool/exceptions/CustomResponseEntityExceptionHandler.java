9fbc3c3f5096f0f205d88464f3273291067a1b5d
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-14 23:25:58 +0300

    user registration part2

--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -23,4 +23,10 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
         ProjectNotFoundExceptionResponse response = new ProjectNotFoundExceptionResponse(ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex, WebRequest request){
+        UsernameAlreadyExistsResponse response = new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 }
--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/UserService.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package io.kuzin.ppmtool.services;
 
 import io.kuzin.ppmtool.domain.User;
+import io.kuzin.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.kuzin.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,8 +17,14 @@ public class UserService {
     private BCryptPasswordEncoder encoder;
 
     public User saveUser(User newUser){
-        newUser.setPassword(encoder.encode(newUser.getPassword()));
-        return userRepository.save(newUser);
+
+        try {
+            newUser.setPassword(encoder.encode(newUser.getPassword()));
+            newUser.setUsername(newUser.getUsername());
+            return userRepository.save(newUser);
+        } catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '" + newUser.getUsername() + "' already exists");
+        }
     }
 
 
