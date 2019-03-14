b236d911432e861d443de443ec1185c9c7644038
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-14 23:12:00 +0300

    user registration part1

--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/UserService.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package io.kuzin.ppmtool.services;
 
+import io.kuzin.ppmtool.domain.User;
 import io.kuzin.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +11,14 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(encoder.encode(newUser.getPassword()));
+        return userRepository.save(newUser);
+    }
 
 
 }
--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/UserController.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/UserController.java
@@ -1,4 +1,37 @@
 package io.kuzin.ppmtool.web;
 
+import io.kuzin.ppmtool.domain.User;
+import io.kuzin.ppmtool.services.MapValidationErrorService;
+import io.kuzin.ppmtool.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.validate(result);
+        if(errorMap != null) return errorMap;
+
+        User newUser = userService.saveUser(user);
+
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
 }
