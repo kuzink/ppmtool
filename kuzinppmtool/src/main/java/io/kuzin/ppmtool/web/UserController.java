d75c3286814dfcd74b6091beaba19c6d22e01638
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-16 16:30:47 +0300

    token generated

--- kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/UserController.java
+++ kuzinppmtool/src/main/java/io/kuzin/ppmtool/web/UserController.java
@@ -1,18 +1,26 @@
 package io.kuzin.ppmtool.web;
 
 import io.kuzin.ppmtool.domain.User;
+import io.kuzin.ppmtool.payload.JWTLoginSucessResponse;
+import io.kuzin.ppmtool.payload.LoginRequest;
+import io.kuzin.ppmtool.security.JwtTokenProvider;
 import io.kuzin.ppmtool.services.MapValidationErrorService;
 import io.kuzin.ppmtool.services.UserService;
 import io.kuzin.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
+import static io.kuzin.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +35,13 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
 
@@ -38,6 +53,25 @@ public class UserController {
         User newUser = userService.saveUser(user);
 
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.validate(result);
+        if(errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSucessResponse(true, jwt));
     }
 
 }
