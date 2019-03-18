295ae66478042647be74ade09938cd9c3ceefc3e
Kanstantsin Kuzin <kuzink11@gmail.com>
2019-03-19 00:03:15 +0300

    intro to securing the react app, security components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -11,6 +11,9 @@ import UpdateProject from "./components/project/UpdateProject";
 import ProjectBoard from "./components/projectboard/ProjectBoard";
 import AddProjectTask from "./components/projectboard/projecttasks/AddProjectTask";
 import UpdateProjectTask from "./components/projectboard/projecttasks/UpdateProjectTask";
+import Landing from "./components/layout/Landing";
+import Register from "./components/usermanagement/Register";
+import Login from "./components/usermanagement/Login";
 
 class App extends Component {
   render() {
@@ -19,6 +22,19 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+
+            {
+              //Public Routes
+            }
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              //Private Routes
+            }
+
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
